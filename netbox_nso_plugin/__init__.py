3cf5956c21716454e2e26132e3a9178b9732a83e
semantic-release <semantic-release>
2026-09-17 12:57:15 +0000

    chore(release): 1.3.1 [skip ci]

--- netbox_nso_plugin/__init__.py
+++ netbox_nso_plugin/__init__.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2025 Marcin Zieba <marcinpsk@gmail.com>
 from netbox.plugins import PluginConfig
 
-__version__ = "1.3.0"
+__version__ = "1.3.1"
 
 
 class NSOPluginConfig(PluginConfig):
